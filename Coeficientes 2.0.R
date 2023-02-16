143a1539b749484be13e66a06484fa70ca106e1f
LeoPerVic <perezvictorinoleonardo@gmail.com>
2023-02-16 10:54:36 -0800

    vectores listos

--- Coeficientes 2.0.R
+++ Coeficientes 2.0.R
@@ -23,14 +23,7 @@ subsec_zm <- datos %>% group_by(CVE_ZM, cve_sub) %>% summarize(po=sum(po))
 
 tot_zm <- datos %>% group_by(CVE_ZM) %>% summarize(po=sum(po))
 
-# Agregar columna "po_tot_zm" al dataframe "tot_zm"
 
-tot_zm$po_tot_zm <- aggregate(po ~ CVE_ZM, data = subsec_zm, sum)$po
 
-# Agregar columna "po_subsec_zm" al dataframe "subsec_zm"
-
-subsec_zm$po_subsec_zm <- aggregate(po ~ CVE_ZM + cve_sub, data = subsec_zm, sum)$po
-
-geo, cve_sub = datos_combinados$cve_sub, operacion = operacion)
 
 
