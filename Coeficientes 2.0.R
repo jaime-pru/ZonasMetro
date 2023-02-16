c38830bd33f69ca3033ec3248f52486a1e8338c2
LeoPerVic <perezvictorinoleonardo@gmail.com>
2023-02-16 10:30:49 -0800

    afinar detalles

--- Coeficientes 2.0.R
+++ Coeficientes 2.0.R
@@ -22,3 +22,32 @@ subsec_zm <- datos %>% group_by(CVE_ZM, cve_sub) %>% summarize(po=sum(po))
 # Crear vector tot_zm
 
 tot_zm <- datos %>% group_by(CVE_ZM) %>% summarize(po=sum(po))
+
+# Agregar columna "po_tot_zm" al dataframe "tot_zm"
+
+tot_zm$po_tot_zm <- aggregate(po ~ CVE_ZM, data = subsec_zm, sum)$po
+
+# Agregar columna "po_subsec_zm" al dataframe "subsec_zm"
+
+subsec_zm$po_subsec_zm <- aggregate(po ~ CVE_ZM + cve_sub, data = subsec_zm, sum)$po
+
+# Realizar la operación
+
+resultado_operacion <- ((tot_zm$po)*(subsec_mun$po)) / ((subsec_zm$po)*tot_mun$po)
+View(resultado_operacion)
+
+# Agregar columna "cve_sub" a "resultados"
+
+resultados <- cbind(resultados, cve_sub = subsec_mun$cve_sub)
+
+# Crear dataframe "resultados"
+
+resultados <- data.frame(cvegeo = subsec_mun$cvegeo, operacion = resultado_operacion)
+
+
+View(resu)
+
+
+# Agregar los resultados al dataframe "resultados"
+
+resultados$operacion <- resultado_operacion
