3ed538c2b68eb8a30be1a06a09cca1bb22b978c6
LeoPerVic <perezvictorinoleonardo@gmail.com>
2023-02-17 10:49:39 -0800

    AVANCE

--- Coeficientes 2.0.R
+++ Coeficientes 2.0.R
@@ -9,26 +9,77 @@ datos <- read_excel("C:\\Users\\rpm0a\\OneDrive\\Documentos\\RepTemplates\\Zonas
 
 # Crear vector subsec_mun
 
-subsec_mun <- datos %>% group_by(cvegeo, cve_sub) %>% summarize(po=sum(po))
+subsec_mun <- datos %>% group_by(cvegeo, CVE_ZM, cve_sub, CVE_ZM) %>% summarize(po=sum(po))
+
 
 # Crear vector tot_mun
 
-tot_mun <- datos %>% group_by(cvegeo) %>% summarize(po=sum(po))
+tot_mun <- datos %>% group_by(CVE_ZM, cvegeo) %>% summarize(po=sum(po))
 
 # Crear vector subsec_zm
 
 subsec_zm <- datos %>% group_by(CVE_ZM, cve_sub) %>% summarize(po=sum(po))
 
+
 # Crear vector tot_zm
 
 tot_zm <- datos %>% group_by(CVE_ZM) %>% summarize(po=sum(po))
 
-# Operacion para estimar QL
 
-resultados <- data.frame(cvegeo = subsec_mun$cvegeo, cve_sub = subsec_mun$cve_sub)
+# Combinar data frames
+
+combinado <- full_join(subsec_mun, tot_mun, by = "cvegeo") %>%
+  full_join(subsec_zm, by = c("cve_sub", "CVE_ZM"))
+
+# Calcular división
+
+combinado <- combinado %>%
+  mutate(division = po.x / po.y)
+
+# Seleccionar columnas de interés
+
+combinado <- combinado %>% select(cvegeo, CVE_ZM, cve_sub, division)
+View(combinado)
+
+#Guardar xlsx
+
+library(openxlsx)
+
+write.xlsx(combinado, "COMBINADO.xlsx")
+
+
+
+
+
+
+
+
+
+
+# Crear vector resultado (QL)
+
+resultado <- (subsec_mun$po / tot_mun$po) / (subsec_zm$po / tot_zm$po)
+
+# Combinar los resultados en un data frame
+
+resultados <- data.frame(cvegeo = subsec_mun$cvegeo, cve_sub = subsec_mun$cve_sub, QL = resultado)
+
 View(resultados)
 
-resultados_operacion <- subsec_mun %>%
-  group_by(cvegeo, cve_sub) %>%
-  summarize(operacion = sum(tot_zm * po_subzec_mun) / sum(subsec_zm * po_subsec_zm * tot_mun$po)) %>%
-  ungroup()
+#Guardar xlsx
+
+library(openxlsx)
+
+write.xlsx(resultados, "resultados.xlsx")
+
+
+# Crear vector resultado (PR)
+
+PR <- (subsec_mun$po / subsec_zm$po) 
+
+# Combinar los resultados en un data frame
+
+PR <- data.frame(cvegeo = subsec_mun$cvegeo, cve_sub = subsec_mun$cve_sub, operacion = resultado)
+
+View(PR)
+
