7ae30ea072b67605f6bd1cf5075d03d09f97ee55
LeoPerVic <perezvictorinoleonardo@gmail>
2023-02-18 16:41:37 -0600

    QL listo

--- Coeficientes 2.0.R
+++ Coeficientes 2.0.R
@@ -5,12 +5,12 @@ library(dplyr)
 
 # Leer datos desde un archivo xlsx
 
-datos <- read_excel("C:\\Users\\rpm0a\\OneDrive\\Documentos\\RepTemplates\\ZonasMetro\\Bases temporales\\BL_zm99.xlsx")
+datos <- read_excel("C:\\Users\\leope\\Documents\\RepTemplates\\ZonasMetro\\Bases temporales\\BL_zm99.xlsx")
 
 # Crear vector subsec_mun
 
-subsec_mun <- datos %>% group_by(cvegeo, CVE_ZM, cve_sub, CVE_ZM) %>% summarize(po=sum(po))
-
+subsec_mun <- datos %>% group_by(cvegeo, CVE_ZM, cve_sub) %>% summarize(po=sum(po))
+View(subsec_mun)
 
 # Crear vector tot_mun
 
@@ -30,6 +30,7 @@ tot_zm <- datos %>% group_by(CVE_ZM) %>% summarize(po=sum(po))
 subsec_mun_div <- left_join(subsec_mun, tot_mun, by = c("cvegeo" = "cvegeo", "CVE_ZM" = "CVE_ZM")) %>% 
   mutate(po = po.x/po.y) %>% 
   select(-po.x, -po.y)
+View(subsec_mun_div)
 
 # Denominador
 
@@ -43,13 +44,28 @@ View(subsec_tot_zm_div)
 
 # Resultado final QL
 
+# Ordenar subsec_mun_div por cvegeo, CVE_ZM y cve_sub
+subsec_mun_div <- subsec_mun_div %>% arrange(cvegeo, CVE_ZM, cve_sub)
 
+# Ordenar subsec_tot_zm_div por CVE_ZM y cve_sub
+subsec_tot_zm_div <- subsec_tot_zm_div %>% arrange(CVE_ZM, cve_sub)
+
+# Unir subsec_mun_div y subsec_tot_zm_div por CVE_ZM y cve_sub
+df <- left_join(subsec_mun_div, subsec_tot_zm_div, by = c("CVE_ZM", "cve_sub"))
+
+# Dividir po de subsec_mun_div entre po_div de subsec_tot_zm_div 
+df <- df %>% mutate(QL = po / po_div)
+
+View(df)
+
+# Seleccionar columnas deseadas
+df <- df %>% select(cvegeo, CVE_ZM, cve_sub, po, po_div)
 
 
 #Guardar xlsx
 
 library(openxlsx)
 
-write.xlsx(resultados, "resultados.xlsx")
+write.xlsx(df, "resultados2.xlsx")
 
 
