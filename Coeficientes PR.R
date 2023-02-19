fc6162da3ed3c7b1aaf964cac37bd7dc5d14a7fb
LeoPerVic <perezvictorinoleonardo@gmail>
2023-02-18 19:55:05 -0600

    PR listos

--- Coeficientes PR.R
+++ Coeficientes PR.R
@@ -28,35 +28,18 @@ subsec_zm <- datos %>% group_by(CVE_ZM, cve_sub) %>% summarize(ue = sum(ue, na.r
                                                                va = sum(va, na.rm = TRUE))
 
   
+# Crear vector con la división de las variables correspondientes
 
-# Numerador
+PR <- subsec_mun %>% 
+  left_join(subsec_zm, by = c("cve_sub", "CVE_ZM")) %>% 
+  mutate(PRue = ue.x / ue.y,
+         PRaf = af.x / af.y,
+         PRfb = fb.x / fb.y,
+         PRpb = pb.x / pb.y,
+         PRpo = po.x / po.y,
+         PRre = re.x / re.y,
+         PRva = va.x / va.y) %>% 
+  select(cvegeo, cve_sub, CVE_ZM, PRue, PRaf, PRfb, PRpb, PRpo, PRre, PRva)
 
-subsec_mun_div <- left_join(subsec_mun, tot_mun, by = c("cvegeo" = "cvegeo", "CVE_ZM" = "CVE_ZM")) %>% 
-  mutate(ue = ue.x/ue.y, af = af.x/af.y, fb = fb.x/fb.y, pb = pb.x/pb.y, po = po.x/po.y, re = re.x/re.y, va = va.x/va.y) %>% 
-  select(-ue.x, -ue.y, -af.x, -af.y, -fb.x, -fb.y, -pb.x, -pb.y, -po.x, -po.y, -re.x, -re.y, -va.x, -va.y)
+View(PR)
 
-# Denominador
-
-# Unir vectores subsec_zm y tot_zm por CVE_ZM
-
-subsec_tot_zm <- left_join(subsec_zm, tot_zm, by = "CVE_ZM")
-
-# Dividir los valores de subsec_zm entre los valores de tot_zm por CVE_ZM
-
-subsec_tot_zm_div <- subsec_tot_zm %>%
-  mutate(ue.div = ue.x/ue.y, af.div = af.x/af.y, fb.div = fb.x/fb.y, pb.div = pb.x/pb.y, po.div = po.x/po.y, re.div = re.x/re.y, va.div = va.x/va.y) %>% 
-  select(-ue.x, -ue.y, -af.x, -af.y, -fb.x, -fb.y, -pb.x, -pb.y, -po.x, -po.y, -re.x, -re.y, -va.x, -va.y)
-View(subsec_tot_zm_div)
-
-# Resultado final QL
-
-# Unir subsec_mun_div y subsec_tot_zm_div por CVE_ZM y cve_sub
-
-QL <- left_join(subsec_mun_div, subsec_tot_zm_div, by = c("CVE_ZM", "cve_sub"))
-
-# Dividir cada variable de subsec_mun_div entre la variable correspondiente de subsec_tot_zm_div
-
-QL <- QL %>% mutate(QLue = ue / ue.div, QLaf = af / af.div, QLfb = fb/fb.div, QLpb = pb/pb.div, QLpo = po/po.div, QLre= re/re.div, QLva = va/va.div) %>% 
-  select(-ue, -ue.div, -af, -af.div, -fb, -fb.div, -fb, -fb.div, -pb, -pb.div, -po, -po.div, -re, -re.div, -va, -va.div)
-
-View(QL)
