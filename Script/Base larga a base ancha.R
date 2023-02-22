6d5e3512001240734ddd8aaa5d0b1a54fa12f7fb
LeoPerVic <perezvictorinoleonardo@gmail.com>
2023-02-22 09:52:48 -0800

    ZM14 CORREGIDA

--- Script/Base larga a base ancha.R
+++ Script/Base larga a base ancha.R
@@ -1,17 +1,19 @@
 # Cargar Librerias
 
+library(readxl)
 library(dplyr)
 library(tidyr)
 
 # Cargar archivo excel
 
-datos <- read_excel("C:\\Users\\leope\\Documents\\RepTemplates\\ZonasMetro\\Bases temporales\\Bases Largas Finales\\BLzm99_final.xlsx")
+datos <- read_excel("C:\\Users\\rpm0a\\OneDrive\\Documentos\\RepTemplates\\ZonasMetro\\Bases temporales\\Bases Largas Finales\\BLzm14_final.xlsx")
 
 # Seleccionar solo las columnas relevantes
 df <- datos %>% 
   select(cvegeo, CVE_ZM, NOM_ZM, cv_mun, nom_mun, cv_ent, nom_ent, cve_sub, 
          ue, af, fb, pb, po, re, va, QLue, QLaf, QLfb, QLpb, QLpo, QLre, QLva, 
-         PRue, PRaf, PRfb, PRpb, PRpo, PRre, PRva)
+         PRue, PRaf, PRfb, PRpb, PRpo, PRre, PRva, HHue, HHaf, HHfb, HHpb, HHpo, 
+         HHre, HHva, IHHue, IHHaf, IHHfb, IHHpb, IHHpo, IHHre, IHHva)
 
 # Obtener valores únicos de cvegeo
 
@@ -22,15 +24,17 @@ df_distinct <- df %>% distinct(cvegeo)
 df_wide <- df %>% 
   pivot_wider(names_from = cve_sub, values_from = c("ue", "af", "fb", "pb", "po", "re", "va", 
                                                     "QLue", "QLaf", "QLfb", "QLpb", "QLpo", "QLre", "QLva", 
-                                                    "PRue", "PRaf", "PRfb", "PRpb", "PRpo", "PRre", "PRva"), 
+                                                    "PRue", "PRaf", "PRfb", "PRpb", "PRpo", "PRre", "PRva",
+                                                    "HHue", "HHaf", "HHfb", "HHpb", "HHpo", "HHre", "HHva",
+                                                    "IHHue", "IHHaf", "IHHfb", "IHHpb", "IHHpo", "IHHre", "IHHva"), 
               names_glue = "{.value}_{cve_sub}")
 
 # Unir valores únicos de cvegeo con datos agrupados por cve_sub
 
-zm99 <- left_join(df_distinct, df_wide, by = "cvegeo")
-View(zm99)
+zm14 <- left_join(df_distinct, df_wide, by = "cvegeo")
+View(zm14)
 
 # Guardar como archivo xlsx
 
 library(openxlsx)
-write.xlsx(zm99, "zm99.xlsx")
+write.xlsx(zm14, "zm14.xlsx")
